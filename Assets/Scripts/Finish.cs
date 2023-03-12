fea8794b0e8be2868732b223b2785606a5dfbac3
Kostia1023 <kostia1023@gmail.com>
2023-03-12 14:34:22 +0200

    .

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{ 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 direction = transform.position - GameObject.FindGameObjectWithTag("Player").transform.position;
+
+            other.GetComponent<PlayerMovement>().speed = 0;
+            Debug.Log("You won!");
+
+        }
+    }
+}
